1f8678f6214f196d13f107d6132bb262efcfc1a8
name <minaary@mailfence.com>
2022-09-07 17:17:15 +0300

    add shuffle function

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -9,17 +9,22 @@ const gifs = importAll(require.context('../gifs/', false, /\.(gif)$/));
 
 export const Deck = () => {
 
-    //const shuffleDeck
+    const shuffle = (array) => {return array.sort(() => Math.random() - 0.5)};
+    
 
+    // array of 20 gifs
     const deck = gifs.map(elem => 
         <img key={elem.toString()} src={elem} />
     );
-    const doSth = () => {console.log('sup')};
+
+    const doSth = () => console.log('sup');
 
     let pickedCards = [];
 
-    const displayedCards = pickedCards.length < 2 ? deck.filter(elem => deck.indexOf(elem) < 3) : doSth();
-    console.log(displayedCards);
+    // display 3 cards to pick from
+    // and shuffle the deck
+    // display unique cards the first two rounds, then display a mix of already picked and not picked yet cards
+    const displayedCards = pickedCards.length < 2 ? shuffle(deck).filter(elem => deck.indexOf(elem) < 3) : doSth();
 
     return (
         <div>
