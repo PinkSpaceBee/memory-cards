bfb5192c7c807600e143ede6f70e6ad58117e6b5
name <minaary@mailfence.com>
2022-09-07 12:22:50 +0300

    add the gifs

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -11,13 +11,20 @@ export const Deck = () => {
 
     //const shuffleDeck
 
-    const deck = gifs.map((elem) => 
+    const deck = gifs.map(elem => 
         <img key={elem.toString()} src={elem} />
     );
+    const doSth = () => {console.log('sup')};
+
+    let pickedCards = [];
+
+    const displayedCards = pickedCards.length < 2 ? deck.filter(elem => deck.indexOf(elem) < 3) : doSth();
+    console.log(displayedCards);
 
     return (
         <div>
-            {deck}
+            {displayedCards}
         </div>
     )
 }
+
