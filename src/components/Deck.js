3cd52722e00b96b73cbe12421749e7eebaef866b
name <minaary@mailfence.com>
2022-09-09 12:19:16 +0300

    change displayed cards on click

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -19,25 +19,23 @@ export const Deck = (props) => {
     const [cardsPicked, setCardsPicked] = useState([]);
 
     // shuffle the deck every time the page is refreshed
-    useEffect(() => {
-        setDisplayedCards(shuffle(deck).slice(0,3));
-    }, []);
+    // useEffect(() => {
+    //     setDisplayedCards(shuffle(deck).slice(0,3));
+    // }, []);
 
     const shuffle = (array) => {return array.sort(() => Math.random() - 0.5)};
 
-
     const pickCard = (e) => {
-        setCardsPicked(cardsPicked => [...cardsPicked, e.target]);
+        setCardsPicked(cardsPicked => [...cardsPicked, e]);
         console.log(cardsPicked);
     }
 
-    // function pickCard(e) {
-    //     console.log(e.target);
-    // }
-
+    const updateDisplayedCards = () => {
+        setDisplayedCards(shuffle(deck).slice(0,3));
+    }
 
     return (
-        <div onClick={pickCard}>
+        <div onClick={(e) => {pickCard(e.target); updateDisplayedCards()}}>
             {displayedCards}
         </div>
     )
