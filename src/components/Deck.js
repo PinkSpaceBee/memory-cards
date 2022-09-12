890fc7951818fa8eecb4bddbea46eea99023d13c
name <minaary@mailfence.com>
2022-09-12 13:09:22 +0300

    remove redundant functions

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 // import all gifs
 function importAll(r) {
@@ -23,30 +23,36 @@ export const Deck = (props) => {
     const [unplayedCards, setUnplayedCards] = useState(deck);
 
 
-    const shuffle = (array) => {return array.sort(() => Math.random() - 0.5)};
+    const shuffle = (array) => {array.sort(() => Math.random() - 0.5)};
+
+    useEffect(() => {
+        shuffle(deck);
+    });
 
     const pickCard = (e) => {
         setCardsPicked(cardsPicked => [...cardsPicked, e]);
+        console.log(cardsPicked);
     }
 
-    const updateDisplayedCards = () => {
+    // some minor changes: first, it seems fucking kudicrous to write a separate function to set state to unplayed cards. Isn't the updateDisplayedCards is like a container function to smaller functions? Besides, setUnplayedCards IS a function so it's not like I'm violating a single responsibility principle here. 
+    const updateDisplayedCards = (e) => {
+        const gifKey = e.outerHTML.substring(10, e.outerHTML.length - 2);
+
         if (cardsPicked.length < 2) {
-            setDisplayedCards(shuffle(deck).slice(0,3));
+            setDisplayedCards(deck.slice(0,3));
+            setUnplayedCards(unplayedCards.filter(elem => elem.key !== gifKey));
         } else {
+            setUnplayedCards(unplayedCards.filter(elem => elem.key !== gifKey));
             console.log('sup bitches');
+            console.log(cardsPicked);
         }
     }
 
-    // okay the question is should I mutate the deck or is it better to create a subarray of unplayed (unpicked) cards and use it to display cards? I meeeeean mutating the deck is probably not a good idea?? hmmmm
-    const test = (e) => {
-        const gifKey = e.outerHTML.substring(10, e.outerHTML.length - 2);
-        setUnplayedCards(unplayedCards.filter(elem => elem.key !== gifKey));
-        console.log(unplayedCards);
-    }
 
+    // okay the question is should I mutate the deck or is it better to create a subarray of unplayed (unpicked) cards and use it to display cards? I meeeeean mutating the deck is probably not a good idea?? hmmmm
 
     return (
-        <div onClick={(e) => {pickCard(e.target); test(e.target); updateDisplayedCards()}}>
+        <div onClick={(e) => {pickCard(e.target); updateDisplayedCards(e.target)}}>
             {displayedCards}
         </div>
     )
